ad4fd1b4a95db306ca4768da7fb656d44702d880
Namprik <wareesa1847@gmail.com>
2024-10-18 23:44:10 +0700

    set style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Mitr", "sans-serif"],
+      },
+      colors: {
+        placeholder: "#9CA3AF",
+        label: "#6B7280", // gray-500
+        text: "black",
+        error: "#EF4444", // red-500
+        selected: "#2578D3",
+      },
+    },
   },
   plugins: [],
 };
