61ad3144507b88ef71f85f164c549db52514af98
Namprik <wareesa1847@gmail.com>
2024-10-20 01:35:02 +0700

    edit password error

--- src/pages/SignUp/SignUpPage.tsx
+++ src/pages/SignUp/SignUpPage.tsx
@@ -74,7 +74,6 @@ export default function SignUpPage() {
               <TextInput
                 id="email"
                 placeholder="Email*"
-                className="w-[400px]"
                 additionalValidation={{
                   required: { value: true },
                   pattern: {
@@ -92,7 +91,6 @@ export default function SignUpPage() {
               <PasswordInput
                 id="password"
                 placeholder="Password*"
-                className="w-[400px]"
                 additionalValidation={{
                   required: { value: true },
                   validate: (value: string) => value === watch("rePassword"),
@@ -110,7 +108,7 @@ export default function SignUpPage() {
                 additionalValidation={{
                   required: { value: true },
                   validate: (value: string) =>
-                    value === watch("password") || "Passwords do not match!",
+                    value === watch("password") || "Password do not match!",
                 }}
               />
             </div>
