fd8fb164888338a15fcc96d658c09f260a8e2e70
Namprik <wareesa1847@gmail.com>
2024-10-22 16:47:31 +0700

    update common component

--- src/commons/components/Input/PasswordInput.tsx
+++ src/commons/components/Input/PasswordInput.tsx
@@ -23,35 +23,37 @@ const PasswordInput: React.FC<InputProps> = ({
   };
 
   return (
-    <div className="relative">
-      <input
-        id={id}
-        placeholder={placeholder}
-        type={isPasswordVisible ? "text" : "password"}
-        className={clsx(
-          "h-[50px] w-full pl-3 pr-[60px] bg-white border font-light placeholder:text-placeholder rounded-md focus:border-selected focus:outline-none disabled:cursor-not-allowed disabled:bg-[#F3F4F6] disabled:border-[#F3F4F6] disabled:text-label",
-          {
-            "border-error": !!errors[id], // Apply red border if there's an error
-            "border-[#DFE4EA]": !errors[id], // Default border color
-          },
-          className
-        )}
-        disabled={disabled}
-        {...register(id, { ...additionalValidation })}
-        autoComplete="off"
-      />
+    <div>
+      <div className="relative">
+        <input
+          id={id}
+          placeholder={placeholder}
+          type={isPasswordVisible ? "text" : "password"}
+          className={clsx(
+            "h-[50px] w-full pl-3 pr-[60px] bg-white border font-light placeholder:text-placeholder rounded-md focus:border-selected focus:outline-none disabled:cursor-not-allowed disabled:bg-[#F3F4F6] disabled:border-[#F3F4F6] disabled:text-label",
+            {
+              "border-error": !!errors[id], // Apply red border if there's an error
+              "border-[#DFE4EA]": !errors[id], // Default border color
+            },
+            className
+          )}
+          disabled={disabled}
+          {...register(id, { ...additionalValidation })}
+          autoComplete="off"
+        />
 
-      <button
-        type="button"
-        onClick={togglePasswordVisibility}
-        className="absolute inset-y-0 right-3 flex items-center z-20 cursor-pointer text-gray-400"
-      >
-        {isPasswordVisible ? (
-          <Icon icon="carbon:view" className="size-6" />
-        ) : (
-          <Icon icon="carbon:view-off" className="size-6" />
-        )}
-      </button>
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute inset-y-0 right-3 flex items-center z-20 cursor-pointer text-gray-400"
+        >
+          {isPasswordVisible ? (
+            <Icon icon="carbon:view" className="size-6" />
+          ) : (
+            <Icon icon="carbon:view-off" className="size-6" />
+          )}
+        </button>
+      </div>
 
       {errors[id]?.message && (
         <span className="font-light text-error text-xs">
