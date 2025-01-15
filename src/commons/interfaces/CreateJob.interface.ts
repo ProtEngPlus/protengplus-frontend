f8e7ccbf088b4299256eb623d09f56d56d719335
Namprik <wareesa1847@gmail.com>
2025-01-15 10:34:19 +0700

    [pep-16] add job and create-job interface

--- src/commons/interfaces/CreateJob.interface.ts
+++ src/commons/interfaces/CreateJob.interface.ts
@@ -1,4 +1,14 @@
-import { OptionValue } from "./Job.interface";
+import { InputMode, OptionValue, RunType } from "./Job.interface";
+
+export interface CreateJobDetailInterface {
+  name: string;
+  description: string;
+  run_type: RunType;
+  is_notification_on: boolean;
+  input_protein: string;
+  input_mode: InputMode;
+  lab_result: { sequence: string; score: number }[];
+}
 
 export interface CreateJobInterface {
   [method: string]: {
@@ -11,4 +21,9 @@ export interface CreateJobInterface {
 export interface PipelineItem {
   method: string;
   subMethod: string;
+}
+
+export interface PipelineItems {
+  method: string;
+  subMethod: string[];
 }
--- src/commons/interfaces/Job.interface.ts
+++ src/commons/interfaces/Job.interface.ts
@@ -1,28 +1,42 @@
+export type RunType = "auto" | "one-step";
+
+export type State = "Created" | "Pending" | "Ongoing" | "Failed" | "Completed";
+
+export type InputMode = "uniprot_id" | "prot_seq";
+
 export type LabResult = {
   total: number;
   sequences: string[];
   scores: number[];
 };
 
-export type OptionValue = string | string[] | number | number[];
-export type RunType = "auto" | "one-step";
+export type OptionValue =
+  | string
+  | string[]
+  | number
+  | number[]
+  | boolean
+  | undefined;
 
-export interface Job {
+export interface JobInterface {
+  ref_job_id?: string;
+  user_id: string;
+  stage_id: number;
+  input_mode: InputMode;
   name: string;
   description: string;
+  input_protein: string;
   run_type: RunType;
-  isNotificationOn: boolean;
-  state: string;
-  stage_id: number;
+  is_notification_on: boolean;
   lab_result: LabResult;
+  meta: string[];
   options: Record<string, Record<string, OptionValue>>;
   artifact: Record<string, object> | null;
-  meta: string[];
-  input_protein: string;
 }
 
-export interface JobResponse extends Job {
+export interface JobResponse extends JobInterface {
   id: string;
+  state: State;
 }
 
 export interface JobSearchParams {
