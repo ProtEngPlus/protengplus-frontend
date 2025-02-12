7a210831bb60cab98c26d8af74406bebf7516367
Namprik <wareesa1847@gmail.com>
2025-02-12 14:49:14 +0700

    feat: add job and create job interface

---
+++ src/commons/interfaces/CreateJob.interface.ts
@@ -0,0 +1,50 @@
+import { OptionValue, RunType, State } from "./Job.interface";
+
+export const stepsForCreateJob = [
+  "Protein Query",
+  "Protein Representation",
+  "Upload Lab Input",
+  "Top Model",
+  "Mutation",
+  "Conclusion",
+];
+
+export interface PipelineItem {
+  method: string;
+  subMethod: string;
+}
+
+export interface PipelineItems {
+  method: string;
+  subMethod: string[];
+}
+
+export interface CreateJobDetail {
+  state: State;
+  stage_id: number;
+  name: string;
+  description: string;
+  input_protein: string;
+  meta: string[];
+  lab_result: { sequence: string; score: number }[]; // to add in table and export csv easier
+  run_type: RunType;
+  is_notification_on: boolean;
+  ref_job_id?: string;
+  artifact: Record<string, object> | null;
+}
+
+// option that send to backend
+export interface CreateJobOption {
+  [subMethod: string]: {
+    [key: string]: OptionValue;
+  };
+}
+
+// option that use on website
+export interface JobOption {
+  [method: string]: {
+    [subMethod: string]: {
+      [key: string]: OptionValue;
+    };
+  };
+}
---
+++ src/commons/interfaces/Job.interface.ts
@@ -0,0 +1,44 @@
+export type RunType = "auto" | "one-step";
+
+export type State = "CREATED" | "PENDING" | "ONGOING" | "FAILED" | "COMPLETED";
+
+export const Steps = [
+  "Protein Query",
+  "Protein Representation",
+  "Top Model",
+  "Mutation",
+];
+
+export type LabResult = {
+  total: number;
+  sequences: string[];
+  scores: number[];
+};
+
+export type OptionValue =
+  | string
+  | string[]
+  | number
+  | number[]
+  | boolean
+  | undefined;
+
+export interface JobInterface {
+  id: string;
+  user_id: string;
+  stage_id: number;
+  state: State;
+  ref_job_id?: string;
+  name: string;
+  description: string;
+  input_protein: string;
+  meta: string[];
+  lab_result: LabResult;
+  options: Record<string, Record<string, OptionValue>>;
+  is_notification_on: boolean;
+  run_type: RunType;
+  artifact: Record<string, object> | null;
+  error_logs: string[];
+  created_at: string;
+  complete_at?: string;
+}
