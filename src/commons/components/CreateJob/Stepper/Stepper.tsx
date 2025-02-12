a82f2d941bbde3248fcf1d6360ab943eac3a5bca
Namprik <wareesa1847@gmail.com>
2025-02-12 17:34:11 +0700

    fix: change parameter's name

--- src/commons/components/CreateJob/Stepper/Stepper.tsx
+++ src/commons/components/CreateJob/Stepper/Stepper.tsx
@@ -1,10 +1,10 @@
 import { stepsForCreateJob } from "../../../interfaces/CreateJob.interface";
 export default function Stepper({
-  state,
-  changeState,
+  step,
+  changeStep,
 }: {
-  state: number;
-  changeState: (n: number) => void;
+  step: number;
+  changeStep: (n: number) => void;
 }) {
   return (
     // start at i = 1
@@ -18,19 +18,19 @@ export default function Stepper({
                 ? "flex w-full relative text-black after:content-[''] after:w-full after:h-0.5 after:inline-block after:absolute lg:after:top-5 after:top-3 after:left-10"
                 : "flex relative text-gray-900"
             } ${
-              i < state - 1 ? "after:bg-pep-blue" : "after:bg-pep-gray-border"
+              i < step - 1 ? "after:bg-pep-blue" : "after:bg-pep-gray-border"
             }`}
           >
             <div className="block whitespace-nowrap z-10">
               <span
                 className={`w-6 h-6 border-2 rounded-full flex justify-center items-center mx-auto mb-3 text-sm lg:w-10 lg:h-10 ${
-                  i < state - 1
+                  i < step - 1
                     ? "bg-pep-blue text-white border-transparent cursor-pointer"
-                    : i === state - 1
+                    : i === step - 1
                     ? "bg-white border-pep-blue text-pep-blue"
                     : "bg-white border-pep-gray-border text-black"
                 }`}
-                onClick={() => state > i && changeState(i + 1)}
+                onClick={() => step > i && changeStep(i + 1)}
               >
                 {i + 1}
               </span>
