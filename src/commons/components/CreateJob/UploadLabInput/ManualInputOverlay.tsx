1ddd6f88182c5c4fefd2fbf05553e43dc36982db
Namprik <wareesa1847@gmail.com>
2025-02-13 00:31:43 +0700

    Reapply "feat: edit create job detail interface"
    This reverts commit f6001115cd2d7e0d6ac15e6ab64ae0b6e86a5432.

---
+++ src/commons/components/CreateJob/UploadLabInput/ManualInputOverlay.tsx
@@ -0,0 +1,142 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Button from "../../Button/Button";
+import { Modal } from "flowbite";
+import type { ModalOptions, ModalInterface } from "flowbite";
+import { useEffect, useState } from "react";
+import Textarea from "../InputField/InputField/Textarea";
+import { useFormContext } from "react-hook-form";
+import { LabManualInputTable } from "./LabInputTable";
+
+export type ManualInputOverlayProps = {
+  onClose: () => void;
+  onConfirm: (data: { sequence: string; score: number }[]) => void;
+};
+
+export function ManualInput({
+  setProteinVisible,
+  isVisible,
+  manualInputProps,
+}: {
+  setProteinVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  isVisible: boolean;
+  manualInputProps: ManualInputOverlayProps;
+}) {
+  const { onClose, onConfirm } = manualInputProps;
+  const { watch } = useFormContext();
+  const inputManual = watch("lab-result-manual");
+  const inputProtein = watch("input_protein");
+  const [data, setData] = useState<{ sequence: string; score: number }[]>([]);
+
+  useEffect(() => {
+    const $modalElement = document.querySelector(`manual-input`);
+    let modal: ModalInterface | null = null;
+
+    if ($modalElement instanceof HTMLElement && isVisible) {
+      const modalOptions: ModalOptions = {
+        placement: "bottom-right",
+        backdrop: "dynamic",
+        backdropClasses:
+          "bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-10",
+        closable: true,
+        onHide: () => console.log("Confirm modal is hidden"),
+        onShow: () => console.log("Confirm modal is shown"),
+      };
+
+      modal = new Modal($modalElement, modalOptions);
+      modal.show();
+    }
+
+    return () => {
+      if (modal) {
+        modal.hide();
+      }
+    };
+  }, [isVisible]);
+
+  const handleAdd = () => {
+    // Split the string into lines
+    const lines = inputManual.trim().split("\n");
+
+    // Map each line into an object with sequence and score
+    const newData = lines.map((line: any) => {
+      const [sequence, score] = line.split(",").map((item: any) => item.trim());
+      return { sequence, score: parseFloat(score) };
+    });
+
+    setData(newData);
+  };
+
+  return (
+    isVisible && (
+      <div
+        id="manual-input"
+        tabIndex={-1}
+        className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-gray-900/50 mt-0"
+      >
+        <div className="space-y-5">
+          <div className="flex bg-pep-blue-light opacity-100 px-8 py-5 gap-x-8 min-w-fit w-[90%] m-auto z-[70] text-center">
+            <div className="grow place-items-start space-y-5 mt-14 bg-white p-3 rounded-xl h-fit">
+              <div className="flex space-x-3">
+                <Icon
+                  icon="hugeicons:dna"
+                  className="size-6 min-w-6 min-h-6 text-pep-gray"
+                />
+                <label className="font-light">Input:</label>
+              </div>
+              <Textarea
+                id="lab-result-manual"
+                className="!w-full !min-w-[500px] !min-h-[220px]"
+                placeholder={`Protein Sequence, Score\nProtein Sequence, Score\nProtein Sequence, Score`}
+                additionalValidation={{
+                  maxLength: {
+                    value: 50,
+                  },
+                }}
+              />
+              <Button
+                id="add-input"
+                buttonType="submit"
+                type="button"
+                text="Add"
+                className="w-fit px-3 py-2 font-normal inline-flex whitespace-nowrap text-center gap-3 text-pep-dark-gray"
+                onClick={handleAdd}
+              >
+                <Icon icon="basil:add-outline" className="size-6" />
+              </Button>
+            </div>
+            <div className="space-y-8">
+              <div className="flex space-x-2">
+                <span className="text-pep-dark-gray font-normal">Total:</span>
+                <span className="font-light text-pep-blue">
+                  {data.length} sequences
+                </span>
+              </div>
+              <LabManualInputTable
+                setProteinVisible={setProteinVisible}
+                inputProtein={inputProtein}
+                data={data}
+                setData={setData}
+              />
+            </div>
+          </div>
+          <div className="flex py-5 space-x-16 place-self-center">
+            <Button
+              id="cancel"
+              buttonType="cancel"
+              text="Cancel"
+              className="w-[190px] min-w-fit"
+              onClick={onClose}
+            />
+            <Button
+              id="confirm"
+              buttonType="next"
+              text="Upload"
+              className="w-[190px] min-w-fit font-light"
+              onClick={() => onConfirm(data)}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
--- src/pages/CreateJob/component/CreateJobOption/CreateJob.tsx
+++ src/pages/CreateJob/component/CreateJobOption/CreateJob.tsx
@@ -1,299 +1,40 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
 import Button from "../../../../commons/components/Button/Button";
-import { JobInterface } from "../../../../commons/interfaces/Job.interface";
-import { useEffect, useMemo, useState } from "react";
-import SelectInput from "../../../../commons/components/Input/SelectInput";
-import { FormProvider, useForm, useWatch } from "react-hook-form";
 
-const mockData: JobInterface[] = [
-  {
-    id: "672850f8f90bb0327c9dc7d4",
-    state: "COMPLETED",
-    name: "New Job#1",
-    description: "this is a job#1",
-    stage_id: 0,
-    user_id: "65d100cf306894794f89017f",
-    lab_result: {
-      total: 24,
-      sequences: [
-        "ASIQHFHW",
-        "CSIQHFHW",
-        "DSIQHFHW",
-        "ESIQHFHW",
-        "FSIQHFHW",
-        "GSIQHFHW",
-        "HSIQHFHW",
-        "ISIQHFHW",
-        "JSIQHFHW",
-        "KSIQHFHW",
-        "LSIQHFHW",
-        "MSIQHFHW",
-        "NSIQHFHW",
-        "OSIQHFHW",
-        "PSIQHFHW",
-        "QSIQHFHW",
-        "RSIQHFHW",
-        "SSIQHFHW",
-        "TSIQHFHW",
-        "USIQHFHW",
-        "VSIQHFHW",
-        "WSIQHFHW",
-        "XSIQHFHW",
-        "YSIQHFHW",
-      ],
-      scores: [
-        0.0029140000697225332, 0.003019999945536256, 0.0022189998999238014,
-        0.004379000049084425, 0.0029140000697225332, 0.003019999945536256,
-        0.0022189998999238014, 0.004379000049084425, 0.0029140000697225332,
-        0.003019999945536256, 0.0022189998999238014, 0.004379000049084425,
-        0.0029140000697225332, 0.003019999945536256, 0.0022189998999238014,
-        0.004379000049084425, 0.0029140000697225332, 0.003019999945536256,
-        0.0022189998999238014, 0.004379000049084425, 0.0029140000697225332,
-        0.003019999945536256, 0.0022189998999238014, 0.004379000049084425,
-      ],
-    },
-    options: {
-      blast: {
-        program: "blastp",
-        hitlist_size: 50,
-        database: "nr",
-        expect: 10,
-        perc_ident: 100,
-        random_state: 50,
-        seq_length: 70,
-        hsp_cov: 0,
-      },
-      unirep: {
-        n_trials: 2,
-        n_splits: 2,
-        n_epochs_config_low: 1,
-        n_epochs_config_high: 11,
-        learning_rate_config_low: 0.00001,
-        learning_rate_config_high: 0.0001,
-      },
-      ridgecv: {
-        train_batch_sizes: [24, 64, 96],
-        n_batch: 12,
-        alpha: 0.1,
-      },
-      mutation: {
-        temperature: 0.01,
-        num_iterations: 25,
-        num_trajectories: 5,
-        mutate_pos_range: 8,
-      },
-    },
-    meta: ["blast", "unirep", "ridgecv", "mutation"],
-    artifact: null,
-    input_protein: "MLDDYDQS",
-    run_type: "auto",
-    is_notification_on: true,
-    created_at: "",
-    error_logs: [],
-  },
-  {
-    id: "672850f8f90bb0327c9dc7d3",
-    state: "COMPLETED",
-    name: "New Job#2",
-    description: "this is a job#2",
-    stage_id: 0,
-    user_id: "65d100cf306894794f89017f",
-    lab_result: {
-      total: 24,
-      sequences: [
-        "ASIQHFHW",
-        "CSIQHFHW",
-        "DSIQHFHW",
-        "ESIQHFHW",
-        "FSIQHFHW",
-        "GSIQHFHW",
-        "HSIQHFHW",
-        "ISIQHFHW",
-        "JSIQHFHW",
-        "KSIQHFHW",
-        "LSIQHFHW",
-        "MSIQHFHW",
-        "NSIQHFHW",
-        "OSIQHFHW",
-        "PSIQHFHW",
-        "QSIQHFHW",
-        "RSIQHFHW",
-        "SSIQHFHW",
-        "TSIQHFHW",
-        "USIQHFHW",
-        "VSIQHFHW",
-        "WSIQHFHW",
-        "XSIQHFHW",
-        "YSIQHFHW",
-      ],
-      scores: [
-        0.0029140000697225332, 0.003019999945536256, 0.0022189998999238014,
-        0.004379000049084425, 0.0029140000697225332, 0.003019999945536256,
-        0.0022189998999238014, 0.004379000049084425, 0.0029140000697225332,
-        0.003019999945536256, 0.0022189998999238014, 0.004379000049084425,
-        0.0029140000697225332, 0.003019999945536256, 0.0022189998999238014,
-        0.004379000049084425, 0.0029140000697225332, 0.003019999945536256,
-        0.0022189998999238014, 0.004379000049084425, 0.0029140000697225332,
-        0.003019999945536256, 0.0022189998999238014, 0.004379000049084425,
-      ],
-    },
-    options: {
-      blast: {
-        program: "blastp",
-        hitlist_size: 50,
-        database: "nr",
-        expect: 10,
-        perc_ident: 100,
-        random_state: 50,
-        seq_length: 70,
-        hsp_cov: 0,
-      },
-      unirep: {
-        n_trials: 2,
-        n_splits: 2,
-        n_epochs_config_low: 1,
-        n_epochs_config_high: 11,
-        learning_rate_config_low: 0.00001,
-        learning_rate_config_high: 0.0001,
-      },
-      ridgecv: {
-        train_batch_sizes: [24, 64, 96],
-        n_batch: 12,
-        alpha: 0.1,
-      },
-      mutation: {
-        temperature: 0.01,
-        num_iterations: 25,
-        num_trajectories: 5,
-        mutate_pos_range: 8,
-      },
-    },
-    meta: ["blast", "unirep", "ridgecv", "mutation"],
-    artifact: null,
-    input_protein: "MLDDYDQS",
-    run_type: "auto",
-    is_notification_on: true,
-    created_at: "",
-    error_logs: [],
-  },
-];
-
-type FormValues = {
-  selectConfigJob: string;
-  selectStep: string;
-};
-
-export default function CreateJobWithConfig({
-  onCancel,
-  onConfirm,
+export default function CreateJob({
+  createJob,
+  createJobWithConfig,
 }: {
-  onCancel: () => void;
-  onConfirm: (configJob: JobInterface | undefined, step: string) => void;
+  createJob: () => void;
+  createJobWithConfig: () => void;
 }) {
-  const form = useForm<FormValues>();
-  const selectedJobId = useWatch({
-    control: form.control,
-    name: "selectConfigJob",
-  });
-
-  const [jobConfigs, setJobConfigs] = useState<JobInterface[]>([]);
-  const [isLoading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchJobs = async () => {
-      setLoading(true);
-      try {
-        const data = mockData;
-        setJobConfigs(data);
-      } catch (err) {
-        console.log("Failed to load configurations.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchJobs();
-  }, []);
-
-  const configurationOptions = useMemo(() => {
-    return jobConfigs.map((job) => ({
-      label: job.name,
-      value: job.id,
-    }));
-  }, [jobConfigs]);
-
-  const selectedJob = useMemo(() => {
-    return jobConfigs.find((job) => job.id === selectedJobId);
-  }, [selectedJobId, jobConfigs]);
-
   return (
     <div className="h-full place-content-center">
-      <FormProvider {...form}>
-        <div className="w-full max-w-[665px] p-16 rounded-lg border border-pep-gray-border place-self-center space-y-16">
-          <h1 className="text-pep-blue text-4xl leading-8 text-center text-nowrap">
-            Create Job With Configuration
-          </h1>
-          <div className="space-y-3">
-            <div className="grid grid-cols-[2fr,3fr] items-center gap-3 font-light">
-              <label className="text-nowrap">Select Configuration:</label>
-              {isLoading ? (
-                <div>Loading configuration</div>
-              ) : (
-                <SelectInput
-                  id="selectConfigJob"
-                  options={configurationOptions}
-                  additionalValidation={{ required: { value: true } }}
-                />
-              )}
-              <label className="self-start text-nowrap">Description:</label>
-              {selectedJob && (
-                <label className="text-wrap">{selectedJob.description}</label>
-              )}
-            </div>
-            <div className="shrink text-pep-dark-gray">
-              <h1 className="my-3">Used Existing Model</h1>
-              <label className="font-light">
-                To expedite the process, you may choose the "Continue" step,
-                which utilizes the existing training results, or you can restart
-                the entire workflow by selecting step 1 - "Protein Query."
-              </label>
-            </div>
-            <hr />
-            <div className="grid grid-cols-[2fr,3fr] items-center gap-3 font-light ">
-              <label className="text-nowrap">Continue at Step:</label>
-              <SelectInput
-                id="selectStep"
-                defaultValue="3"
-                options={[
-                  { label: "Step 1 - Protein Query", value: "1" },
-                  { label: "Step 2 - Protein Representation", value: "2" },
-                  { label: "Step 3 - Upload Lab Input", value: "3" },
-                  { label: "Step 2 - Top Model", value: "4" },
-                  { label: "Step 5 - Mutation", value: "5" },
-                ]}
-                additionalValidation={{ required: { value: true } }}
-              />
-            </div>
-          </div>
-          <div className="flex justify-between gap-2">
-            <Button
-              id="btn-create-job"
-              buttonType="cancel"
-              text="Back"
-              className=""
-              onClick={onCancel}
-            />
-            <Button
-              id="btn-create-config-job"
-              buttonType="next"
-              text="Next"
-              className=""
-              onClick={() =>
-                onConfirm(selectedJob, form.getValues("selectStep"))
-              }
-            />
-          </div>
+      <div className="p-16 rounded-lg border border-pep-gray-border place-self-center space-y-16">
+        <h1 className="text-pep-blue text-4xl leading-8 text-center">
+          Create Job
+        </h1>
+        <div className="flex flex-col w-fit space-y-9 place-content-center place-items-center">
+          <Button
+            id="btn-create-job"
+            buttonType="submit"
+            text="Create New Job"
+            className="min-w-fit w-full !font-light text-2xl inline-flex items-center whitespace-nowrap place-content-center text-center gap-2"
+            onClick={createJob}
+          >
+            <Icon icon="basil:add-outline" className="size-6" />
+          </Button>
+          <Button
+            id="btn-create-config-job"
+            buttonType="submit"
+            text="Create Job With Configuration"
+            className="w-fit !font-light text-2xl inline-flex items-center whitespace-nowrap place-content-center text-center gap-2"
+            onClick={createJobWithConfig}
+          >
+            <Icon icon="basil:add-outline" className="size-6" />
+          </Button>
         </div>
-      </FormProvider>
+      </div>
     </div>
   );
 }
