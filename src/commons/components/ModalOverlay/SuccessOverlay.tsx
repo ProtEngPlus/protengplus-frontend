0a6d16fd2ee8926bc2a98698f74549ef20f6a432
Namprik <wareesa1847@gmail.com>
2025-02-12 14:10:41 +0700

    style: edit modal overlay

--- src/commons/components/ModalOverlay/SuccessOverlay.tsx
+++ src/commons/components/ModalOverlay/SuccessOverlay.tsx
@@ -30,8 +30,6 @@ export function SuccessOverlay({
         backdropClasses:
           "bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-10",
         closable: true,
-        onHide: () => console.log("Confirm modal is hidden"),
-        onShow: () => console.log("Confirm modal is shown"),
       };
 
       modal = new Modal($modalElement, modalOptions);
@@ -40,7 +38,7 @@ export function SuccessOverlay({
 
     return () => {
       if (modal) {
-        modal.hide(); // Hide the modal if it was instantiated
+        modal.hide();
       }
     };
   }, [isVisible]);
@@ -50,8 +48,7 @@ export function SuccessOverlay({
       <div
         id={`#success-modal-${id}`}
         tabIndex={-1}
-        aria-hidden="true"
-        className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full bg-gray-900/50"
+        className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-gray-900/50"
       >
         <div className="modal-container bg-white space-y-9 rounded-lg px-8 py-5 w-fit m-auto shadow-dropShadow z-[100 text-center]">
           <img src={successIcon} alt="Success Icon" className="mx-auto" />
@@ -60,6 +57,7 @@ export function SuccessOverlay({
             <Button
               id="accept"
               buttonType="cancel"
+              type="button"
               text="Accept"
               className="w-[190px] min-w-fit"
               onClick={onClose}
