8969f618a57e84ebfabf3c863ae79b1297064239
Namprik <wareesa1847@gmail.com>
2025-02-14 01:40:26 +0700

    feat: add configuration for create job page

--- src/commons/interfaces/CreateJob.interface.ts
+++ src/commons/interfaces/CreateJob.interface.ts
@@ -21,6 +21,8 @@ export interface PipelineItems {
 
 export interface CreateJobInterface {
   user_id: string;
+  stage_id: number;
+  state: State;
   ref_job_id?: string;
   name: string;
   description: string;
@@ -58,20 +60,4 @@ export interface JobOption {
       [key: string]: OptionValue;
     };
   };
-}
-
-export interface JobConfiguration {
-  state: State;
-  id: string;
-  user_id: string;
-  ref_job_id: string;
-  name: string;
-  description: string;
-  input_protein: string;
-  meta: string[];
-  lab_result: LabResult;
-  options: Record<string, Record<string, OptionValue>>;
-  is_notification_on: boolean;
-  run_type: RunType;
-  artifact: Record<string, object>;
 }
