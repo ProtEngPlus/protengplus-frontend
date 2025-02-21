ea2db1df65fe09c2c10c80e4430ca8532a44f0d1
matikant <fe-2003@hotmail.com>
2025-02-21 13:37:53 +0700

    fix: tailwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,8 +29,6 @@ export default {
         "pep-light-gray": "#F1F1F1",
         "pep-blue-light": "#EFF6FF",
         "pep-gray-light": "#F9FAFB",
-        "pep-dark-blue": "#165BAA",
-        "pep-light-gray": "#F1F1F1",
       },
       boxShadow: {
         dropShadow: "0 4px 4px 0 rgba(0,0,0,0.25)",
