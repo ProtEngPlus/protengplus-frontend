7705435208fd56b8a58c688ad5e7142e7da6644e
Namprik <wareesa1847@gmail.com>
2025-02-25 22:52:31 +0700

    feat: add upload input tab

---
+++ src/pages/JobDetail/components/LabInput/LabInput.tsx
@@ -0,0 +1,314 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { useEffect, useRef, useState } from "react";
+import Papa, { ParseResult } from "papaparse";
+import { useFormContext } from "react-hook-form";
+import {
+  ConfirmOverlay,
+  ConfirmOverlayProps,
+} from "../../../../commons/components/ModalOverlay/ConfirmOverlay";
+import {
+  ManualInput,
+  ManualInputOverlayProps,
+} from "../../../../commons/components/CreateJob/UploadLabInput/ManualInputOverlay";
+import {
+  InputProteinOverlay,
+  InputProteinOverlayProps,
+} from "../../../../commons/components/CreateJob/InputProteinOverlay/InputProteinOverlay";
+import DownloadCSVButton from "../../../../commons/components/Button/DownloadCSVButton";
+import { UploadInputTemplate } from "../../../CreateJob/services/UploadInputTemplate";
+import { LabInputTable } from "../../../../commons/components/CreateJob/UploadLabInput/LabInputTable";
+import Button from "../../../../commons/components/Button/Button";
+import { LabResult } from "../../../../commons/interfaces/Job.interface";
+
+export const exampleLabResult = [
+  { sequence: "ASIQHFHW", score: 0.002914 },
+  { sequence: "CSIQHFHW", score: 0.00302 },
+  { sequence: "DSIQHFHW", score: 0.002219 },
+  { sequence: "ESIQHFHW", score: 0.004379 },
+];
+
+export default function UploadLabInput({ disable }: { disable: boolean }) {
+  const { getValues } = useFormContext();
+  const minimumSize = getValues(`options.RidgeCV`)?.[0];
+  const labData: LabResult = getValues("lab_result");
+  const inputProtein = getValues("input_protein");
+  const labResult = labData.sequences.map((sequence, index) => ({
+    sequence,
+    score: labData.scores[index],
+  }));
+
+  const [isExpand, setIsExpand] = useState(false);
+  const [isRead, setRead] = useState(false);
+  const [labResultForm, setLabResultForm] = useState<
+    { sequence: string; score: number }[]
+  >(labResult ?? []);
+  const [file, setFile] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isError, setIsError] = useState({
+    isValidate: true,
+    errorMessage: "",
+  });
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = event.target.files;
+    if (fileList && fileList.length > 0) {
+      setFile(fileList[0]);
+    }
+  };
+
+  const checkValidate = (data: { sequence: string; score: number }[]) => {
+    // Regular expression for valid amino acid sequences
+    const validSequenceRegex = /^(?!.*[BJOUXZ])[A-Z]+$/i;
+
+    if (data.length < minimumSize) {
+      return {
+        isValidate: false,
+        errorMessage: `Total sequences should match minimum training batch size !\nMinimum training batch size can be modify in Step-4 “Top Model”`,
+      };
+    }
+
+    for (const { sequence, score } of data) {
+      if (
+        !sequence ||
+        score == null ||
+        isNaN(score) ||
+        !validSequenceRegex.test(sequence) ||
+        sequence.toUpperCase().includes("ATGC") ||
+        (sequence !== sequence.toUpperCase() &&
+          sequence !== sequence.toLowerCase())
+      ) {
+        return {
+          isValidate: false,
+          errorMessage: "Uploaded file mismatch!",
+        };
+      }
+    }
+
+    return { isValidate: true, errorMessage: "" };
+  };
+
+  useEffect(() => {
+    if (file) {
+      Papa.parse(file, {
+        header: false,
+        skipEmptyLines: true,
+        delimiter: ",",
+        complete: (results: ParseResult<string[]>) => {
+          const fileData = results.data.map((line: any) => {
+            return { sequence: line[1], score: line[2] };
+          });
+          const validation = checkValidate(fileData);
+          setIsError(validation);
+
+          if (validation.isValidate) {
+            setLabResultForm(fileData);
+          }
+        },
+      });
+    }
+  }, [file]);
+
+  const [isConfirmVisible, setConfirmVisible] = useState(false);
+  const ConfirmProps: ConfirmOverlayProps = {
+    id: "confirm-manual-input",
+    onClose: () => setConfirmVisible(false),
+    onConfirm: () => {
+      setConfirmVisible(false);
+      setManualInputVisible(true);
+    },
+    title: "Do you want to discard the current uploaded file?",
+    message:
+      "The current uploaded file will be permanently removed and cannot be recovered.",
+  };
+
+  const [isManualInputVisible, setManualInputVisible] = useState(false);
+  const ManualInputProps: ManualInputOverlayProps = {
+    onClose: () => setManualInputVisible(false),
+    onConfirm: (data) => {
+      const validation = checkValidate(data);
+      setIsError(validation);
+      if (validation.isValidate) {
+        setLabResultForm(data);
+      }
+      setManualInputVisible(false);
+    },
+  };
+
+  const [isProteinVisible, setProteinVisible] = useState(false);
+  const InputProteinProps: InputProteinOverlayProps = {
+    inputProtein: inputProtein,
+    onClose: () => {
+      setProteinVisible(false);
+    },
+  };
+
+  return (
+    <div>
+      <ConfirmOverlay
+        confirmProps={ConfirmProps}
+        isVisible={isConfirmVisible}
+      />
+      <InputProteinOverlay
+        isVisible={isProteinVisible}
+        inputProteinProps={InputProteinProps}
+      />
+      <ManualInput
+        setProteinVisible={setProteinVisible}
+        isVisible={isManualInputVisible}
+        manualInputProps={ManualInputProps}
+      />
+      <div className="space-y-11 rounded-lg border border-pep-gray-border px-6 py-8">
+        {/*------------------------------------ Lab Input Template ----------------------------------*/}
+        {labResult.length == 0 && (
+          <div className="space-y-6 font-light">
+            <div className="flex border-l-4 border-pep-orange px-6 text-xl">
+              Lab Input Template
+            </div>
+            <hr />
+            <div className="flex justify-between space-x-4">
+              <Icon
+                icon={isExpand ? "mingcute:up-line" : "mingcute:down-line"}
+                onClick={() => setIsExpand(!isExpand)}
+                className="cursor-pointer text-pep-gray size-[30px]"
+              />
+              <DownloadCSVButton onClick={() => UploadInputTemplate()} />
+            </div>
+            <div className="rounded-lg bg-pep-blue-light p-5 text-pep-dark-gray">
+              {isExpand ? (
+                <LabInputTable data={exampleLabResult} isExample={true} />
+              ) : (
+                "Click expand to see full template"
+              )}
+            </div>
+          </div>
+        )}
+
+        {/*------------------------------------ Upload Lab Input ----------------------------------*/}
+        <div className="space-y-6 font-light">
+          <div className="w-[353px] min-w-fit flex justify-between border-l-4 border-pep-orange px-6 font-light text-xl gap-x-5">
+            Upload Lab Input
+            <Icon
+              icon="material-symbols:info-outline"
+              className={`size-8 cursor-pointer ${
+                isRead ? "text-pep-blue" : "text-pep-gray"
+              }
+           
+            `}
+              onClick={() => setRead(!isRead)}
+            />
+          </div>
+          <hr />
+          <div className="relative space-y-8">
+            <div
+              className={`flex space-x-5  ${
+                !isError.isValidate || labResultForm.length == 0
+                  ? "justify-between items-center"
+                  : "justify-end"
+              }`}
+            >
+              {!isError.isValidate ? (
+                <div className="text-error flex flex-row items-center space-x-[10px]">
+                  <div>*</div>
+                  <div
+                    className="text-wrap"
+                    dangerouslySetInnerHTML={{
+                      __html: isError.errorMessage.replace(/\n/g, "<br />"),
+                    }}
+                  ></div>
+                </div>
+              ) : labResult.length === 0 ? (
+                <div>Please upload your lab input</div>
+              ) : undefined}
+
+              <div className="flex space-x-5">
+                {/* --------------------------------Upload lab Button------------------------------------ */}
+                <div>
+                  <input
+                    type="file"
+                    accept=".csv"
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                    className="hidden"
+                  />
+                  <Button
+                    id="upload-lab-input"
+                    type="button"
+                    buttonType="submit"
+                    text="Upload"
+                    className="w-fit font-normal inline-flex items-center whitespace-nowrap place-content-center text-center gap-2 !px-2"
+                    onClick={handleButtonClick}
+                    disabled={disable}
+                  >
+                    <Icon icon="uil:upload" className="size-5" />
+                  </Button>
+                </div>
+
+                {/* -------------------------------------Manual input Button----------------------------------------- */}
+                <Button
+                  id="upload-lab-input-manual"
+                  type="button"
+                  buttonType="submit"
+                  text="Manual Input"
+                  className={`w-fit font-normal inline-flex items-center whitespace-nowrap place-content-center text-center gap-2 !px-2`}
+                  disabled={disable}
+                  onClick={() => setConfirmVisible(true)}
+                >
+                  <Icon
+                    icon="material-symbols-light:keyboard-external-input-rounded"
+                    className="size-5"
+                  />
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative rounded-lg bg-pep-blue-light place-items-center">
+            {/*----------------------------------- Lab Input Table --------------------------------------------*/}
+            {labResult.length > 0 && isError.isValidate && (
+              <div className="w-[738px] space-y-8 p-5 min-w-fit">
+                <div className="flex space-x-2 text-start">
+                  <span className="text-pep-dark-gray font-normal">Total:</span>
+                  <span className="font-light text-pep-blue">
+                    {labResult.length} sequences
+                  </span>
+                </div>
+                <div>
+                  <LabInputTable
+                    data={labResult}
+                    isExample={false}
+                    inputProtein={inputProtein}
+                    setProteinVisible={setProteinVisible}
+                  />
+                </div>
+              </div>
+            )}
+            {/*------------------------------------- Helper Text -------------------------------------------------*/}
+            {isRead && (
+              <div className="absolute inset-0 rounded-lg p-5 z-10 bg-pep-blue-light min-h-fit">
+                <div className="flex space-x-3">
+                  <Icon icon="ep:setting" className="text-pep-gray size-6" />
+                  <label>Parameter Setup</label>
+                </div>
+                <div className="flex mt-4 space-x-5 text-pep-dark-gray items-center">
+                  <span className="text-nowrap">Upload Lab Result:</span>
+                  <span className="leading-10">
+                    Upload your lab results with the experimented protein
+                    sequences and their scores. This data will be used to train
+                    the model for low-n engineering.
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
