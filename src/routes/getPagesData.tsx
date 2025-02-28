ba08f6fc44ec6d7b925cfea42a1e83f7b9feb712
matikant <fe-2003@hotmail.com>
2025-02-28 17:29:54 +0700

    fix: remove ReportApp

--- src/routes/getPagesData.tsx
+++ src/routes/getPagesData.tsx
@@ -11,7 +11,6 @@ import AccountManagementApp from "../pages/AccountManagement/AccountManagementAp
 import ChangePasswordApp from "../pages/ChangePassword/ChangePasswordApp";
 import CreateJobApp from "../pages/CreateJob/CreateJobApp";
 import JobDetailApp from "../pages/JobDetail/JobDetailApp";
-import ReportApp from "../pages/Report/ReportApp";
 
 const pagesData = [
   {
@@ -62,10 +61,6 @@ const pagesData = [
     path: "/dashboard/job-detail/:jobid",
     element: <JobDetailApp />,
   },
-  {
-    path: "/report",
-    element: <ReportApp />,
-  }
 ] as unknown as RouteProps[];
 
 export default pagesData;
