e13a64afd9663d61af165a357ce2511058affdc4
Namprik <wareesa1847@gmail.com>
2025-03-10 02:15:41 +0700

    fix(debug): remove console.log

--- src/pages/JobDetail/JobDetailPage.tsx
+++ src/pages/JobDetail/JobDetailPage.tsx
@@ -46,7 +46,6 @@ export default function JobDetailPage() {
     if (jobid) {
       const data = await getJob(jobid);
       if (data.data) {
-        console.log(data.data);
         setJob(data.data);
         form.reset({
           ...data.data,
