43a21ab377b148df2d4d7f26691ae3b9f77f2f0a
Namprik <wareesa1847@gmail.com>
2025-03-10 01:07:57 +0700

    feat: add query result's filter

---
+++ src/commons/components/CreateJob/InputProtein/FilterOverlay/FilterOverlay.tsx
@@ -0,0 +1,166 @@
+import { Modal } from "flowbite";
+import type { ModalOptions, ModalInterface } from "flowbite";
+import { useEffect, useState } from "react";
+import { createJobConfig } from "../../../../configs/createJobConfig";
+import GetInputField from "../../InputField/GetInputField";
+import { useFormContext } from "react-hook-form";
+import Button from "../../../Button/Button";
+import SearchBar from "./SearchBar";
+
+export type FilterOverlayProps = {
+  organismList: string[];
+  organisms: string | undefined;
+  percentIdentityFrom: number | undefined;
+  percentIdentityTo: number | undefined;
+  eValuesFrom: number | undefined;
+  eValuesTo: number | undefined;
+  queryCoverFrom: number | undefined;
+  queryCoverTo: number | undefined;
+  onClose: () => void;
+  onConfirm: () => void;
+  onReset: () => void;
+};
+
+export function FilterOverlay({
+  isVisible,
+  filterOverlayProps,
+}: {
+  isVisible: boolean;
+  filterOverlayProps: FilterOverlayProps;
+}) {
+  const {
+    organismList,
+    organisms,
+    percentIdentityFrom,
+    percentIdentityTo,
+    eValuesFrom,
+    eValuesTo,
+    queryCoverFrom,
+    queryCoverTo,
+    onClose,
+    onReset,
+    onConfirm,
+  } = filterOverlayProps;
+  const proteinConfig = createJobConfig["Protein Input"]?.tool["Query Result"];
+
+  const { getValues, setValue } = useFormContext();
+  const [search, setSearch] = useState<string>("");
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const $modalElement = document.querySelector("protein-filter-modal");
+
+    let modal: ModalInterface | null = null;
+
+    if ($modalElement instanceof HTMLElement && isVisible && !isLoading) {
+      const modalOptions: ModalOptions = {
+        placement: "bottom-right",
+        closable: true,
+      };
+
+      modal = new Modal($modalElement, modalOptions);
+      modal.show();
+    }
+
+    return () => {
+      if (modal) {
+        modal.hide();
+      }
+    };
+  }, [isVisible]);
+
+  useEffect(() => {
+    if (isVisible) {
+      setLoading(true);
+      setSearch(organisms ?? "");
+      setValue("percent_identity_result_low", percentIdentityFrom);
+      setValue("percent_identity_result_high", percentIdentityTo);
+      setValue("e_values_result_low", eValuesFrom);
+      setValue("e_values_result_high", eValuesTo);
+      setValue("query_cover_result_low", queryCoverFrom);
+      setValue("query_cover_result_high", queryCoverTo);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 0);
+    }
+  }, [isVisible]);
+
+  const handleConfirm = () => {
+    setValue("organisms", search);
+    setValue(
+      "percentIdentityFrom",
+      getValues("percent_identity_result_low") ?? undefined
+    );
+    setValue(
+      "percentIdentityTo",
+      getValues("percent_identity_result_high") ?? undefined
+    );
+    setValue("eValuesFrom", getValues("e_values_result_low") ?? undefined);
+    setValue("eValuesTo", getValues("e_values_result_high") ?? undefined);
+    setValue(
+      "queryCoverFrom",
+      getValues("query_cover_result_low") ?? undefined
+    );
+    setValue("queryCoverTo", getValues("query_cover_result_high") ?? undefined);
+
+    onConfirm();
+  };
+
+  return (
+    isVisible &&
+    !isLoading && (
+      <div
+        id="protein-filter-modal"
+        className="fixed top-0 right-0 left-0 z-[90] flex justify-center items-center w-full h-full mt-0"
+      >
+        <div className="bg-white p-5 space-y-5 place-items-center place-self-center w-[944px] max-h-[400px] min-w-fit place-content-center text-center">
+          <div className="w-full flex rounded-xl px-5 py-6 border border-pep-gray-border gap-x-4 place-items-start">
+            <div className="grow">
+              <SearchBar
+                search={search}
+                setSearch={setSearch}
+                organismList={organismList}
+              />
+            </div>
+
+            <div className="w-fit text-start">
+              {proteinConfig.parameters.map((value) => (
+                <GetInputField
+                  key={value.id}
+                  id={value.id}
+                  type={value.type}
+                  label={value.name}
+                  options={value.dropdownItems}
+                  value={{
+                    low: undefined,
+                    high: undefined,
+                  }}
+                  additionalValidation={value.additionalValidation}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="space-x-4 w-fit ml-auto">
+            <Button
+              id="reset"
+              type="button"
+              buttonType="cancel"
+              text="Reset"
+              className="h-[34px] items-center !p-0"
+              onClick={onReset}
+            />
+            <Button
+              id="filter"
+              type="button"
+              buttonType="submit"
+              text="Filter"
+              className="h-[34px] items-center !p-0 font-normal"
+              onClick={handleConfirm}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
---
+++ src/commons/components/CreateJob/InputProtein/FilterOverlay/SearchBar.tsx
@@ -0,0 +1,67 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+import searchIcon from "../../../../../assets/images/CreateJob/searchIcon.svg";
+import { useEffect, useState } from "react";
+
+export default function SearchBar({
+  organismList,
+  search,
+  setSearch,
+}: {
+  organismList: string[];
+  search: string;
+  setSearch: (search: string) => void;
+}) {
+  const [filteredList, setFilteredList] = useState<string[]>(organismList);
+
+  const handleSearch = (searchValue: string) => {
+    setSearch(searchValue);
+    const filtered = organismList.filter((organism) =>
+      organism.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredList(filtered);
+  };
+
+  useEffect(() => {
+    handleSearch(search);
+  }, [organismList, search, setSearch]);
+
+  return (
+    <div className="relative w-full flex space-x-2 items-start">
+      <div className="flex text-center place-items-center space-x-2 h-[50px]">
+        <Icon
+          icon="eos-icons:organisms-outlined"
+          className="size-6 text-pep-gray"
+        />
+        <label className="text-center text-nowrap font-light">Organism:</label>
+      </div>
+      <div className="flex flex-col grow">
+        <div className="relative flex items-center grow">
+          <input
+            autoComplete="off"
+            id="organism"
+            value={search}
+            onChange={(e) => handleSearch(e.target.value)}
+            placeholder="Search organism"
+            className="grow text-wrap h-[50px] w-full min-w-fit pl-10 pr-3 bg-white border font-light placeholder:text-placeholder rounded-md focus:border-pep-blue focus:outline-none disabled:cursor-not-allowed disabled:bg-disabled disabled:border-disabled disabled:text-label"
+          />
+          <img
+            src={searchIcon}
+            alt="search"
+            className="absolute top-1/2 left-3 transform -translate-y-1/2 cursor-pointer"
+          />
+        </div>
+        <ul className="mt-2 bg-white rounded-md shadow-dropShadow z-10 py-2 right-0 max-h-[200px] overflow-y-scroll">
+          {filteredList.map((option, index) => (
+            <li
+              key={index}
+              className="text-start px-5 py-2 font-light text-label hover:text-pep-blue hover:bg-blue-50 focus:bg-blue-100 cursor-pointer"
+              onMouseDown={() => handleSearch(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
