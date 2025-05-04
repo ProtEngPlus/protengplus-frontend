6fe43e20b59a58bf1af29fb9c73630f7e5927d84
Namprik <wareesa1847@gmail.com>
2025-05-04 20:23:50 +0700

    feat: update disable button style

--- src/commons/components/Button/Button.tsx
+++ src/commons/components/Button/Button.tsx
@@ -19,12 +19,14 @@ export default function Button({
   ...props
 }: ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>) {
   const baseStyles =
-    "w-48 h-[3.125rem] rounded-md px-7 py-3 disabled:cursor-not-allowed disabled:bg-white disabled:text-black disabled:border disabled:border-pep-gray-border";
+    "w-48 h-[3.125rem] rounded-md px-7 py-3 disabled:cursor-not-allowed";
   const typeStyles = {
-    submit: "font-medium bg-pep-blue hover:bg-pep-blue-hover text-white",
+    submit:
+      "font-medium bg-pep-blue hover:bg-pep-blue-hover text-white disabled:hover:bg-pep-blue",
     cancel:
-      "font-light bg-white hover:bg-blue-50 text-black hover:text-pep-blue border border-pep-gray-border",
-    delete: "font-medium bg-red-500 hover:bg-red-700 text-white",
+      "font-light bg-white hover:bg-blue-50 text-black hover:text-pep-blue border border-pep-gray-border disabled:hover:bg-white disabled:hover:text-black",
+    delete:
+      "font-medium bg-red-500 hover:bg-red-700 text-white disabled:hover:bg-red-500",
     next: "bg-pep-orange text-white",
   };
 
