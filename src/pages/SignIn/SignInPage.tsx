f57dd5c39a04f105e83960bf8f9693b38780fc23
BadLuckZ <thanagorn8802@gmail.com>
2026-09-23 17:13:31 +0700

    fix: sign in, forget password, reset password box

--- src/pages/SignIn/SignInPage.tsx
+++ src/pages/SignIn/SignInPage.tsx
@@ -52,7 +52,7 @@ export default function SignInPage() {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="w-[28%] min-w-fit h-auto py-6 px-4 m-auto bg-white rounded-xl shadow-dropShadow">
+      <div className="w-[36%] min-w-[440px] h-auto py-10 px-8 m-auto bg-white rounded-xl shadow-dropShadow">
         <img src={logoWithText} alt="logo-with-text" className="mb-6 mx-auto" />
         <FormProvider {...form}>
           <form
